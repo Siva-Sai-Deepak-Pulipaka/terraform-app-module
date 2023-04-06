fa91cd4ae7bcc6f0804c5d33eda769f64c33886d
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-04-07 01:32:49 +0530

    configured app part

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# creating launch template
 resource "aws_launch_template" "main" {
   name = "${var.component}-${var.env}"
 
@@ -33,6 +34,7 @@ resource "aws_launch_template" "main" {
       } ))
 }
 
+# security groups for the instances 
 resource "aws_security_group" "main" {
   name        = "${var.component}-${var.env}"
   description = "${var.component}-${var.env}"
@@ -70,12 +72,14 @@ resource "aws_security_group" "main" {
   )
 }
 
+# auto scaling group
 resource "aws_autoscaling_group" "main" {
   name = "${var.component}-${var.env}"
   desired_capacity   = var.desired_capacity
   max_size           = var.max_size
   min_size           = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns = [aws_lb_target_group.arn]
 
   launch_template {
     id      = aws_launch_template.main.id
@@ -85,5 +89,49 @@ resource "aws_autoscaling_group" "main" {
     key = "Name"
     propagate_at_launch = false
     value = "${var.component}-${var.env}"
+  }
+}
+
+# target group
+resource "aws_lb_target_group" "main" {
+  name     = "${var.component}-${var.env}"
+  port     = var.port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 5
+    interval = 5
+    timeout = 3
+  }
+  tags = merge(
+    var.tags,
+    { Name = "${var.component}-${var.env}" }
+  ) 
+}
+
+# we are using dns record if we enter www then it will access. Its just an extra feature to look domain comfortably
+resource "aws_route53_record" "main" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = local.dns_name
+  type    = "CNAME"
+  ttl     = 30
+  records = [var.alb_dns_name]
+}
+
+# adding listener rulw
+resource "aws_lb_listener_rule" "listener" {
+  listener_arn = var.listener_arn
+  priority     = var.listener_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = [local.dns_name]
+    }
   }
 }
