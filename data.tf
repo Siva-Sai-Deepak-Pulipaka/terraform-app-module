713d57f936949034502ff15cf1d90bb6510b0de7
deepak <18l11a0518@gmail.com>
2023-04-15 10:52:59 +0530

    changes reverted

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "devops-practice-ansible-new"
+  name_regex       = "devops-practice-ansible"
   owners           = ["self"]
 }
 
